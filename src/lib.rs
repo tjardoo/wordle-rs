3811dad6ae505a1d9fc28997b3899866b2098099
tjardoo <info@tjardo.com>
2023-02-08 10:08:40 +0100

    Used bytes instead of chars to compute correctness

--- src/lib.rs
+++ src/lib.rs
@@ -66,7 +66,7 @@ impl Correctness {
 
         let mut c = [Correctness::Wrong; 5];
 
-        for (i, (a, g)) in answer.chars().zip(guess.chars()).enumerate() {
+        for (i, (a, g)) in answer.bytes().zip(guess.bytes()).enumerate() {
             if a == g {
                 c[i] = Correctness::Correct;
             }
@@ -80,12 +80,12 @@ impl Correctness {
             }
         }
 
-        for (i, g) in guess.chars().enumerate() {
+        for (i, g) in guess.bytes().enumerate() {
             if c[i] == Correctness::Correct {
                 continue;
             }
 
-            if answer.chars().enumerate().any(|(i, a) | {
+            if answer.bytes().enumerate().any(|(i, a) | {
                 if a == g && !used[i] {
                     used[i] = true;
 
